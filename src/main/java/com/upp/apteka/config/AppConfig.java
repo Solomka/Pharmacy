209e2cae7ead37f8451b3a116313b51a093a6fac
Solomka <sol.yaremko@gmail.com>
2017-01-28 23:48:42 +0200

    hibernate + mysql dependencies

--- src/main/java/com/upp/apteka/config/AppConfig.java
+++ src/main/java/com/upp/apteka/config/AppConfig.java
@@ -1,26 +1,49 @@
 package com.upp.apteka.config;
 
+import javax.sql.DataSource;
 import javax.swing.JFrame;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
-@ComponentScan(basePackages = "com.upp")
+@ComponentScan(basePackages = "com.upp.apteka")
 @EnableAspectJAutoProxy
+@PropertySource("classpath:jdbc.properties")
+@Import({HibernateConfig.class})
 public class AppConfig {
 
+	@Autowired
+	private Environment environment;
+
 	@Bean
-	public JFrame getDispatcherFrame(){
+	DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getRequiredProperty("jdbc.databaseurl"));
+		dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+		dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+		return dataSource;
+
+	}
+
+	@Bean
+	public JFrame getDispatcherFrame() {
 		JFrame dispatcherFrame = new JFrame();
-		
+
 		dispatcherFrame.setTitle("Аптека");
 		dispatcherFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		dispatcherFrame.setSize(500, 300);
 		dispatcherFrame.setLocationRelativeTo(null);
-		
+
 		return dispatcherFrame;
 	}
 }
--- src/main/java/com/upp/apteka/config/Mapper.java
+++ src/main/java/com/upp/apteka/config/Mapper.java
@@ -8,12 +8,24 @@ import org.springframework.stereotype.Component;
 
 import com.upp.apteka.controller.SwingController;
 
+/**
+ * Changing activities mapper
+ * 
+ */
 @Component
 public class Mapper {
 	
 	@Autowired
 	private ApplicationContext applicationContext;
 	
+	/**
+	 * method that returns appropriate view representation controller bean and invokes its 'showing activity class method'
+	 * 
+	 * 1 controller per 1 showing activity
+	 * 
+	 * @param name
+	 * @param params
+	 */
 	public void changeActivity(String name, Map<String, Object> params){
 		SwingController swingController = applicationContext.getBean(name, SwingController.class);
 		swingController.switchToActivity(params);
