adc79eaac8c44ad53803e4adc3774be7895255fd
Solomka <sol.yaremko@gmail.com>
2017-01-31 20:27:39 +0200

    repository specific methods

--- src/main/java/com/upp/apteka/repository/PharmacyRepository.java
+++ src/main/java/com/upp/apteka/repository/PharmacyRepository.java
@@ -1,9 +1,13 @@
 package com.upp.apteka.repository;
 
+import java.util.List;
+
 import com.upp.apteka.bo.Pharmacy;
 import com.upp.apteka.utils.repository.HqlSpecification;
 import com.upp.apteka.utils.repository.IRepository;
 
 public interface PharmacyRepository extends IRepository<Pharmacy, Long, HqlSpecification> {
+
+	List<Pharmacy> findPharamcyByName(String name);
 
 }
