3008b6fb80dfdf90f9a2e9e503077a34249afde2
Yanivskiy Oleh <oleh_01@mail.ru>
2017-02-20 23:15:44 +0200

    Add AllDoctors

---
+++ src/main/java/com/upp/apteka/activity/AllDoctors.java
@@ -0,0 +1,292 @@
+package com.upp.apteka.activity;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.upp.apteka.bo.Doctor;
+import com.upp.apteka.config.Mapper;
+import com.upp.apteka.service.DoctorService;
+
+@Component("allDoctorsActivity")
+@Scope("prototype")
+public class AllDoctors implements Activity {
+
+	@Autowired
+	private Mapper mapper;
+
+	@Autowired
+	private JFrame jFrame;
+
+	private JTable doctorsTable;
+
+	private Object[] columnsHeader = new String[] { "Прізвище", "Ім'я", "Спеціальність", "Стаж" };
+
+	private JTextField queryField;
+
+	private static final int BUTTON_HEIGHT = 20;
+	private static final int BUTTON_WIDTH = 120;
+
+	private static final int PAGINATION_BUTTON_HEIGHT = 25;
+	private static final int PAGINATION_BUTTON_WIDTH = 90;
+
+	private static final int ROW_HEIGHT = 32;
+
+	private static final Font font = new Font("SansSerif", Font.PLAIN, 14);
+
+	private List<Doctor> doctors;
+
+	private int lastPage;
+	private int currentPage;
+
+	private String query;
+
+	@Autowired
+	private DoctorService doctorService;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void showActivity(final Map<String, Object> params) {
+
+		JPanel mainPanel = new JPanel();
+		mainPanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
+
+		jFrame.setContentPane(mainPanel);
+
+		doctors = (List<Doctor>) params.get("doctors");
+		jFrame.setLayout(new BorderLayout());
+
+		lastPage = (int) params.get("last");
+		currentPage = (int) params.get("current");
+
+		JPanel searchPanel = new JPanel();
+		searchPanel.setLayout(new BorderLayout());
+		searchPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+		JPanel fieldWrapper = new JPanel();
+		queryField = new JTextField();
+
+		JButton queryButton = new JButton("Шукати");
+		queryButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+		queryField.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT - 5));
+
+		queryButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Map<String, Object> params = new HashMap<>();
+
+				params.put("current", currentPage);
+				params.put("query", queryField.getText());
+
+				mapper.changeActivity("allDoctors", params);
+			}
+		});
+
+		query = (String) params.get("query");
+		queryField.setText(query);
+
+		fieldWrapper.setLayout(new GridLayout(0, 1));
+		fieldWrapper.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		fieldWrapper.add(queryField);
+
+		JPanel contPanel = new JPanel();
+		contPanel.setLayout(new FlowLayout());
+
+		JPanel emptyPanel = new JPanel();
+		emptyPanel.setPreferredSize(new Dimension(30, BUTTON_HEIGHT));
+		searchPanel.add(emptyPanel, BorderLayout.WEST);
+
+		JLabel infoLabel = new JLabel("Сторінка: " + currentPage + "/" + lastPage);
+		infoLabel.setFont(font);
+
+		contPanel.add(infoLabel);
+		contPanel.add(emptyPanel);
+
+		searchPanel.add(contPanel, BorderLayout.WEST);
+		searchPanel.add(fieldWrapper, BorderLayout.CENTER);
+		searchPanel.add(queryButton, BorderLayout.EAST);
+
+		jFrame.add(searchPanel, BorderLayout.NORTH);
+
+		doctorsTable = new JTable(getData(doctors), columnsHeader);
+		doctorsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		doctorsTable.setFont(font);
+		doctorsTable.setRowHeight(ROW_HEIGHT);
+		doctorsTable.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		doctorsTable.getTableHeader().setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+		JScrollPane scrollPane = new JScrollPane(doctorsTable);
+		scrollPane.setBorder(BorderFactory.createLineBorder(jFrame.getBackground()));
+		jFrame.add(scrollPane, BorderLayout.CENTER);
+
+		JPanel paginationPanel = new JPanel();
+		paginationPanel.setLayout(new BorderLayout());
+
+		JButton nextButton = new JButton("Далі");
+		nextButton.setPreferredSize(new Dimension(PAGINATION_BUTTON_WIDTH, PAGINATION_BUTTON_HEIGHT));
+
+		nextButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Map<String, Object> params = new HashMap<>();
+
+				params.put("current", currentPage + 1);
+				params.put("query", query);
+
+				mapper.changeActivity("allDoctors", params);
+			}
+		});
+
+		if (currentPage == lastPage)
+			nextButton.setEnabled(false);
+
+		JButton prevButton = new JButton("Назад");
+		prevButton.setPreferredSize(new Dimension(PAGINATION_BUTTON_WIDTH, PAGINATION_BUTTON_HEIGHT));
+
+		prevButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Map<String, Object> params = new HashMap<>();
+
+				params.put("current", currentPage - 1);
+				params.put("query", query);
+
+				mapper.changeActivity("allDoctors", params);
+			}
+		});
+
+		if (currentPage == 1)
+			prevButton.setEnabled(false);
+
+		JPanel goPanel = new JPanel();
+		final JTextField goTo = new JTextField(10);
+		goPanel.setLayout(new GridLayout(0, 1));
+		goPanel.setPreferredSize(new Dimension(PAGINATION_BUTTON_WIDTH, PAGINATION_BUTTON_HEIGHT));
+		goPanel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		goPanel.add(goTo);
+
+		JButton editButton = new JButton("Змінити");
+		editButton.setPreferredSize(new Dimension(PAGINATION_BUTTON_WIDTH, PAGINATION_BUTTON_HEIGHT));
+
+		editButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int selectedRow = doctorsTable.getSelectedRow();
+
+				if (selectedRow != -1) {
+					Long id = doctors.get(selectedRow).getId();
+
+					Map<String, Object> params = new HashMap<String, Object>();
+					params.put("id", id);
+
+					mapper.changeActivity("addDoctor", params);
+				} else {
+					JOptionPane.showMessageDialog(jFrame, new String[] { "Виберіть спочатку лікаря!" }, "Помилка",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+
+		JButton removeButton = new JButton("Видалити");
+		removeButton.setPreferredSize(new Dimension(PAGINATION_BUTTON_WIDTH, PAGINATION_BUTTON_HEIGHT));
+
+		removeButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int selectedRow = doctorsTable.getSelectedRow();
+
+				if (selectedRow != -1) {
+					Long id = doctors.get(selectedRow).getId();
+
+					boolean success = doctorService.delete(id);
+
+					if (success)
+						mapper.changeActivity("allDoctors", params);
+					else {
+						JOptionPane.showMessageDialog(jFrame, new String[] { "Він взагалі-то рецепти виписував. Так що ні-ні." },
+								"Помилка", JOptionPane.ERROR_MESSAGE);
+					}
+				} else {
+					JOptionPane.showMessageDialog(jFrame, new String[] { "Виберіть спочатку лікаря!" }, "Помилка",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+
+		JButton goButton = new JButton("Перейти");
+		goButton.setPreferredSize(new Dimension(PAGINATION_BUTTON_WIDTH, PAGINATION_BUTTON_HEIGHT));
+		goButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+					int page = Integer.valueOf(goTo.getText());
+					
+					if(page > lastPage)
+						page = lastPage;
+					
+					if(page < 0)
+						page = 0;
+					
+					params.put("current", page);
+					mapper.changeActivity("allDoctors", params);
+				}catch(Exception ex){
+					JOptionPane.showMessageDialog(jFrame, new String[] { "Числа нормальні треба вводити!" }, "Помилка",
+							JOptionPane.ERROR_MESSAGE);
+				}
+				
+			}
+		});
+		
+		paginationPanel.setLayout(new FlowLayout());
+		paginationPanel.add(nextButton, BorderLayout.WEST);
+		paginationPanel.add(goPanel, BorderLayout.WEST);
+		paginationPanel.add(goButton, BorderLayout.WEST);
+		paginationPanel.add(prevButton, BorderLayout.WEST);
+		paginationPanel.add(editButton, BorderLayout.WEST);
+		paginationPanel.add(removeButton, BorderLayout.WEST);
+
+		jFrame.add(paginationPanel, BorderLayout.SOUTH);
+	}
+
+	private Object[][] getData(List<Doctor> doctors) {
+		Object[][] array = new Object[doctors.size()][columnsHeader.length];
+
+		for (int i = 0; i < doctors.size(); i++) {
+			array[i][0] = doctors.get(i).getSurname();
+			array[i][1] = doctors.get(i).getName();
+			array[i][2] = doctors.get(i).getOccupation();
+			array[i][3] = doctors.get(i).getStanding();
+		}
+
+		return array;
+	}
+}
--- src/main/java/com/upp/apteka/config/AppConfig.java
+++ src/main/java/com/upp/apteka/config/AppConfig.java
@@ -134,6 +134,20 @@ public class AppConfig {
 			}
 		});
 		
+		JMenuItem allDoctors = new JMenuItem("Переглянути лікарів");
+		allDoctors.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				Map<String, Object> params = new HashMap<>();
+				params.put("current", 1);
+
+				mapper.changeActivity("allDoctors", params);
+
+			}
+		});
+		
 		JMenuItem allPurchases = new JMenuItem("Переглянути покупки");
 		allPurchases.addActionListener(new ActionListener() {
 
@@ -149,6 +163,7 @@ public class AppConfig {
 		});
 		
 		viewMenu.add(allPatients);
+		viewMenu.add(allDoctors);
 		viewMenu.add(allPurchases);
 
 		addMenu.add(addDoctor);
